f05a59cd582521bedf190c7d03af9d609768fd2f
dxkite <DXkite@163.com>
2020-02-09 12:08:12 +0800

    添加：PNG隐写头和数据长度

--- GoPngSteganography/pngio/base.go
+++ GoPngSteganography/pngio/base.go
@@ -1,23 +1,33 @@
 package pngio
 
 import (
+	"encoding/binary"
 	"image"
 	"io"
 	"math"
 )
 
 type ImagePack struct {
-	Image  *image.NRGBA
-	offset int
+	Image   *image.NRGBA
+	Version uint32
+	Length  uint32
+	offset  int
 }
+
+const (
+	HeadVersion   = 4
+	ContentLength = 4
+	HeaderLength  = HeadVersion + ContentLength
+)
 
 func (p *ImagePack) Write(b []byte) (n int, err error) {
 	n = p.offset
 	r := false
+	h := HeaderLength
 	// 写入数据
 	for _, d := range b {
-		if p.offset < len(p.Image.Pix) {
-			p.Image.Pix[p.offset] = d
+		if p.offset+h < len(p.Image.Pix) {
+			p.Image.Pix[h+p.offset] = d
 		} else {
 			p.Image.Pix = append(p.Image.Pix, d)
 			r = true
@@ -28,7 +38,35 @@ func (p *ImagePack) Write(b []byte) (n int, err error) {
 	if r {
 		p.Resize()
 	}
-	return p.offset - n, nil
+	d := p.offset - n
+	p.Length += uint32(d)
+	p.writeHeader()
+	return d, nil
+}
+
+func (p *ImagePack) writeHeader() {
+	l := make([]byte, 4)
+	i := 0
+	if len(p.Image.Pix) < HeaderLength {
+		p.Image.Pix = make([]byte, HeaderLength)
+	}
+	binary.BigEndian.PutUint32(l, p.Version)
+	p.Image.Pix[i+0] = l[0]
+	p.Image.Pix[i+1] = l[1]
+	p.Image.Pix[i+2] = l[2]
+	p.Image.Pix[i+3] = l[3]
+	binary.BigEndian.PutUint32(l, p.Length)
+	i += 4
+	p.Image.Pix[i+0] = l[0]
+	p.Image.Pix[i+1] = l[1]
+	p.Image.Pix[i+2] = l[2]
+	p.Image.Pix[i+3] = l[3]
+}
+
+func (p *ImagePack) readHeader() (version, length uint32) {
+	version = binary.BigEndian.Uint32(p.Image.Pix[0:4:4])
+	length = binary.BigEndian.Uint32(p.Image.Pix[4:8:8])
+	return
 }
 
 func (p *ImagePack) Read(b []byte) (n int, err error) {
@@ -36,9 +74,11 @@ func (p *ImagePack) Read(b []byte) (n int, err error) {
 		return 0, nil
 	}
 	n = p.offset
+	h := HeaderLength
+	p.Version, p.Length = p.readHeader()
 	for i, _ := range b {
-		if p.offset < len(p.Image.Pix) {
-			b[i] = p.Image.Pix[p.offset]
+		if uint32(p.offset) < p.Length {
+			b[i] = p.Image.Pix[h+p.offset]
 			p.offset++
 		} else {
 			break
@@ -52,19 +92,27 @@ func (p *ImagePack) Read(b []byte) (n int, err error) {
 
 func NewPack(size int) *ImagePack {
 	w, h := getSize(size)
-	return &ImagePack{
-		Image:  image.NewNRGBA(image.Rect(0, 0, w, h)),
-		offset: 0,
+	img := &ImagePack{
+		Image:   image.NewNRGBA(image.Rect(0, 0, w, h)),
+		offset:  0,
+		Version: 1,
+		Length:  0,
 	}
+	img.writeHeader()
+	return img
 }
 
 func NewDataPack(buf []byte) *ImagePack {
 	w, h := getSize(len(buf))
 	r := image.Rect(0, 0, w, h)
-	return &ImagePack{
-		Image:  &image.NRGBA{buf, 4 * w, r},
-		offset: 0,
+	img := &ImagePack{
+		Image:   &image.NRGBA{append(make([]byte, HeaderLength), buf...), 4 * w, r},
+		Version: 1,
+		Length:  uint32(len(buf)),
+		offset:  0,
 	}
+	img.writeHeader()
+	return img
 }
 
 func (p *ImagePack) Resize() *image.NRGBA {
